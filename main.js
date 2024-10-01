67457d6127f1a3434cd52ebbb2af592f2049ada0
ReHab-23 <habermann-r@web.de>
2024-10-01 19:58:03 +0200

    Dateien hinzugefügt

---
+++ main.js
@@ -0,0 +1,32 @@
+/*
+JavaScript-Code hier
+Funktionen etc.
+*/
+
+/*
+PYSCRIPT-CODE
+
+            def fahrkostenrechnung():
+                    kraftstoff = Element('kraftstoff').value
+                    x = float(kraftstoff)
+                    verbrauch = Element('verbrauch').value
+                    y = float(verbrauch)
+                    strecke = Element('strecke').value
+                    z = float(strecke)
+
+                    verbrauchskosten = ((x * y) / 100) * z
+                    Element('output1').write(f"{verbrauchskosten}")
+*/
+
+function zuruecksetzen() {
+    //zurücksetzen der Eingabefelder etc.
+    ergebnis = " ";
+    document.getElementById('output').innerText = ergebnis;
+    //weitere Eingabefelder zurücksetzen
+}
+
+function berechnen() {
+    //Werte aus HTML-Dokument holen
+    //Kosten berechnen
+    //Ergebnis ausgeben
+}
